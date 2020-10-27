54ef89366954f30432439477630a6966b1289a10
Manisha Chaturvedi <68684840+manisha1904@users.noreply.github.com>
2020-10-27 20:47:59 +0530

    Week1

---
+++ Team5/Manisha Chaturvedi/total_negative.cpp
@@ -0,0 +1,21 @@
+/*Program to find total number of negative elements in the given matrix*/
+#include<iostream>
+using namespace std;
+int main(){
+	int m,n;
+	cout<<"Enter the value of number of rows and columns in the matrix"<<endl;
+	cin>>m>>n;
+	int a[m][n],neg=0;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++)
+			cin>>a[i][j];
+	}
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			if(a[i][j]<0)
+				neg++;
+		}
+	}
+	cout<<"Total negative numbers present in the given matrix is "<<neg;
+	return 0;
+}
