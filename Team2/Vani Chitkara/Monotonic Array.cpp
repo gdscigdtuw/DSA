d76aa0998608be498caebfa5be606e37dc2032d1
Vani Chitkara <64951124+vanichitkara@users.noreply.github.com>
2020-10-28 16:39:27 +0530

    Create Monotonic Array.cpp

---
+++ Team2/Vani Chitkara/Monotonic Array.cpp
@@ -0,0 +1,15 @@
+Problem: https://leetcode.com/problems/monotonic-array/
+
+Solution:
+
+bool isMonotonic(vector<int>& A) {
+    bool increasing=true;
+    bool decreasing=true;
+    for(int i=0; i<A.size()-1; ++i){
+        if(A[i]>A[i+1])
+           increasing=false;
+        if(A[i]<A[i+1])
+           decreasing=false;
+    }
+    return increasing||decreasing;
+}
