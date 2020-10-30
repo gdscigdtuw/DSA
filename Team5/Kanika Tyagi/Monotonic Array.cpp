d3a9f519298d2af2c83e79e53529bd35cc9d1999
Kanika-Tyagi <70576709+Kanika-Tyagi@users.noreply.github.com>
2020-10-31 00:30:36 +0530

    Week1, 28 oct

--- Team5/Kanika Tyagi/Monotonic Array.cpp
+++ Team5/Kanika Tyagi/Monotonic Array.cpp
@@ -1,4 +1,4 @@
-/*
+/* By kanika Tyagi
 ues: Monotonic Array
 An array is monotonic if it is either monotone increasing or monotone decreasing.
 An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
