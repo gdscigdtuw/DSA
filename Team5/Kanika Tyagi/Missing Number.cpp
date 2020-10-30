c8553c0953d3d0fb8b3a7e6270430acf907b3d2c
Kanika-Tyagi <70576709+Kanika-Tyagi@users.noreply.github.com>
2020-10-31 00:32:24 +0530

    Week1, 26 oct

--- Team5/Kanika Tyagi/Missing Number.cpp
+++ Team5/Kanika Tyagi/Missing Number.cpp
@@ -1,8 +1,8 @@
-// By Kanika Tyagi 
+/* By Kanika Tyagi 
 Ques: Missing Number
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
-Ans:
+Ans:*/
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
