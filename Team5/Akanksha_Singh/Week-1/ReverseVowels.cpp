4ae3cafa4df148868018de7f849d6a416d339429
Akanksha Singh <akankshasingh121297@gmail.com>
2020-11-02 21:50:54 +0530

    Added reverse vowels in string (345)

---
+++ Team5/Akanksha_Singh/Week-1/ReverseVowels.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isVowel(char e){
+        return e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u'
+            || e == 'A' || e == 'E' || e == 'I' || e == 'O' || e == 'U';
+    }
+    string reverseVowels(string s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(isVowel(s[i])){
+                if(isVowel(s[j])){
+                    swap(s[i], s[j]);
+                    i++;
+                    j--;
+                }else{
+                    j--;
+                }
+            }else{
+                i++;
+            }
+        } 
+        return s;  
+    }
+};
