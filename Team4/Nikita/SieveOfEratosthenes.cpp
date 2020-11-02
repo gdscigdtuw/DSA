4ec7df6bb190e3d59a4654a2363f380e34cb6cd9
nporia <Porianikita262@gmail.com>
2020-11-02 19:28:46 +0530

    Week2

---
+++ Team4/Nikita/SieveOfEratosthenes.cpp
@@ -0,0 +1,33 @@
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        // Write Your Code here
+       vector <int> a;
+       for(int i = 2 ; i <= N ; i++)
+        {
+          int flag = 1;
+          for(int j = 2; j <= sqrt(i) ; j++)
+          {
+            if(i%j != 0)
+            {
+                flag = 1;
+              
+            }
+            else
+            {
+                flag = 0;
+                  break;
+            }
+          }
+           if(flag == 1) 
+           {
+              a.push_back(i);
+           }
+        }
+        
+       return a;
+    }   
+};
