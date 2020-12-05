b91c870bfdc34ff7e0e217a9ecc85369fb879325
Khushboo85277 <khushbooagarwal.jp@gmail.com>
2020-12-05 10:57:46 +0530

    week1/Khushboo

---
+++ Week1/Remove Duplicates/Khushboo.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+            
+        for(int i=1;i<nums.size();i++)
+        {  // cout<<i<<" "<<nums[i]<<" "<<endl;
+            if(nums[i]==nums[i-1])
+            { nums.erase(nums.begin()+i);
+             i--;
+            }
+        }
+        return nums.size();
+        
+    }
+};
