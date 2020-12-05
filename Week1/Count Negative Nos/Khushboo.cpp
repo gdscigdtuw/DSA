a200095d5ca0e6553f9400bf1da9afe2fff204ae
Khushboo85277 <68243350+Khushboo85277@users.noreply.github.com>
2020-12-05 11:45:43 +0530

    week1/Khushboo

--- Week1/Count Negative Nos/Khushboo.cpp
+++ Week1/Count Negative Nos/Khushboo.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
         int count=0;
-        
         int m=grid.size();
         int n=grid[0].size();
-        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -15,8 +13,7 @@ public:
             }
         }
         return count;
-        
-   }
+        }
 };
 
 
